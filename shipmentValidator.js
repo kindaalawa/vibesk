934aaf780fd9288477bbe5b13082d6aeca8dd599
kindaalawa <122926806+kindaalawa@users.noreply.github.com>
2023-11-22 10:14:14 +0200

    Delete shipmentValidator.js

--- shipmentValidator.js
+++
@@ -1,9 +0,0 @@
-const { check } = require('express-validator');
-
-const insertShipmentValidation = [
-  check('order_ID').notEmpty().withMessage('Order ID is required'),
-];
-
-module.exports = {
-  insertShipmentValidation,
-};
