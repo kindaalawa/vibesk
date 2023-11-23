dd4d8ff034c9687a9bdb4ddf1439bbbd5fd5b718
kindaalawa <122926806+kindaalawa@users.noreply.github.com>
2023-11-23 10:55:45 +0200

    Delete validations directory

--- validations/orderValidator.js
+++
@@ -1,9 +0,0 @@
-const { check } = require('express-validator');
-
-const insertOrderValidation = [
-  check('cart_ID').notEmpty().withMessage('Cart ID is required'),
-];
-
-module.exports = {
-  insertOrderValidation,
-};
